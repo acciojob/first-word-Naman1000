dd41e8d3ddeb1498684cf406b2aaa3cbede9cae9
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-09 18:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	let trimmedStr = s.trim();
+    let firstSpaceIndex = trimmedStr.indexOf(" ");
+
+    // If there's no space, return the entire string
+    if (firstSpaceIndex === -1) {
+        return trimmedStr;
+    }
+
+    // Return substring from the start to the first space
+    return trimmedStr.substring(0, firstSpaceIndex);
+	
 }
 
 // Do not change the code below
